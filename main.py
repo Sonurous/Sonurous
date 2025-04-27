de236cc3edbf48c354d7a131546d2f1016c309d3
Sonurous <sprykury@gmail.com>
2025-04-28 06:05:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from xomboo import Xomboo
+from sonurous import sonurous
 
 __CHANNEL_USERNAME__ = "SonuCPMToolshop"
 __GROUP_USERNAME__   = "SonuShop"
